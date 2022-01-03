f99f0cd906646b08515ac8acdd43b7beefd34d03
Elena Gorshenina <elenkagorshenina19.07.14@gmail.com>
2022-01-03 23:30:51 +0300

    Edited build.gradle

--- src/test/java/data/BDclass.java
+++ src/test/java/data/BDclass.java
@@ -13,7 +13,7 @@ public class BDclass {
 
     private static Connection getConn() {
         try {
-            return DriverManager.getConnection("jdbc:mysql://185.119.57.9:8080/app", "app", "pass");
+            return DriverManager.getConnection("jdbc:mysql://185.119.57.9:3306/app", "app", "pass");
         } catch (SQLException e) {
             e.printStackTrace();
         }
